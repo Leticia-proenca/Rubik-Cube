728cab98e3d374f9a642004c23c66bca73231354
leti <leproenca02@icloud.com>
2025-12-20 18:34:17 -0300

    Update function.py

--- function.py
+++ function.py
@@ -47,25 +47,73 @@ class magicCube:
 
             self.pastMovements.append("U'")
 
-        def D(self, inverse=False):
+    def D(self, inverse = False):
         # rotates down face
-            if not inverse:
-                self.rotate_face_clockwise('D')
-                temp = self.faces['F'][2].copy()
+        if not inverse:
+            self.rotateFaces_clockwise('D')
+            temp = self.faces['F'][2].copy()
 
-                self.faces['F'][2] = self.faces['L'][2]
-                self.faces['L'][2] = self.faces['B'][2]
-                self.faces['B'][2] = self.faces['R'][2]
-                self.faces['R'][2] = temp
+            self.faces['F'][2] = self.faces['L'][2]
+            self.faces['L'][2] = self.faces['B'][2]
+            self.faces['B'][2] = self.faces['R'][2]
+            self.faces['R'][2] = temp
 
-                self.move_history.append("D")
-            else:
-                self.rotate_face_counterclockwise('D')
-                temp = self.faces['F'][2].copy()
+            self.pastMovements.append("D")
+        else:
+            self.rotateFaces_counterclockwise('D')
+            temp = self.faces['F'][2].copy()
 
-                self.faces['F'][2] = self.faces['R'][2]
-                self.faces['R'][2] = self.faces['B'][2]
-                self.faces['B'][2] = self.faces['L'][2]
-                self.faces['L'][2] = temp
+            self.faces['F'][2] = self.faces['R'][2]
+            self.faces['R'][2] = self.faces['B'][2]
+            self.faces['B'][2] = self.faces['L'][2]
+            self.faces['L'][2] = temp
 
-                self.move_history.append("'D'")
+            self.pastMovements.append("'D'")
+
+    def R(self, inverse = False):
+            # rotates right face
+        if not inverse:
+            self.rotate_face_clockwise('R')
+            temp = np.array([self.faces['F'][i][2] for i in range(3)])
+
+            for i in range(3):
+                self.faces['F'][i][2] = self.faces['D'][i][2]
+                self.faces['D'][i][2] = self.faces['B'][2-i][0]
+                self.faces['B'][2-i][0] = self.faces['U'][i][2]
+                self.faces['U'][i][2] = temp[i]
+
+            self.pastMovements.append("R")
+        else:
+            self.rotateFaces_counterclockwise('R')
+            temp = np.array([self.faces['F'][i][2] for i in range(3)])
+
+            for i in range(3):
+                self.faces['F'][i][2] = self.faces['U'][i][2]
+                self.faces['U'][i][2] = self.faces['B'][2-i][0]
+                self.faces['B'][2-i][0] = self.faces['D'][i][2]
+                self.faces['D'][i][2] = temp[i]
+
+            self.pastMovements.append("R'")
+
+    def L(self, inverse = False):
+        # rotates left face
+        if not inverse:
+            self.rotateFaces_clockwise('L')
+            temp = np.array([self.faces['F'][i][0] for i in range(3)])
+
+            for i in range(3):
+                self.faces['F'][i][0] = self.faces['D'][i][0]
+                self.faces['D'][i][0] = self.faces['B'][2-i][2]
+                self.faces['B'][2-i][2] = self.faces['U'][i][0]
+                self.faces['U'][i][0] = temp[i]
+            self.pastMovements.append("L")
+        else:
+            self.rotateFaces_counterclockwise('L')
+            temp = np.array([self.faces['F'][i][0] for i in range(3)])
+
+            for i in range(3):
+                self.faces['F'][i][0] = self.faces['D'][i][0]
+                self.faces['D'][i][0] = self.faces['B'][2-i][2]
+                self.faces['B'][2-i][2] = self.faces['U'][i][0]
+                self.faces['U'][i][0] = temp[i]
+            self.pastMovements.append("L'")
