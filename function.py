7ba4a6c90c4a120254269811dcd7637b2f4d2345
leti <leproenca02@icloud.com>
2025-12-20 18:03:11 -0300

    Create function.py

---
+++ function.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import rectangle
+
+class magicCube:
+    def __init__(self):
+        # colours: White, Yellow, Red, Orange, Blue, Green
+        # moves: Up, Down, Front, Back, Left, Right
+
+        self.faces = {'U': np.array([['W']*3 for _ in range(3)]),
+                    'D': np.array([['Y']*3 for _ in range(3)]),
+                    'F': np.array([['R']*3 for _ in range(3)]),
+                    'B': np.array([['O']*3 for _ in range(3)]),
+                    'L': np.arrray([['G']*3 for _ in range(3)]),
+                    'R': np.array([['B']*3 for _ in range(3)])}
+        
+        self.colours = {'W': '#FFFFFF', 'Y': '#FFFF00', 'R': '#FF0000', 'O': '#FF8800', 'B': '#0000FF', 'G': '#00FF00'}
+
+        self.pastMovements = []
+
+    def rotateFaces_clockwise(self ,face):
+        self.faces[face] = np.rot90(self.faces[face], -1) #90 degres rotation
+
+    def rotateFaces_counterclockwise(self, face):
+        self.faces[face] = np.rot90(self.faces[face], 1)
+
+    def U(self, inverse = False):
+        # rotates upper face
+        if not inverse:
+            self.rotateFaces_clockwise('U')
+            temp = self.faces['F'][0].copy()
+
+            self.faces['F'][0] = self.faces['R'][0]
+            self.faces['R'][0] = self.faces['B'][0]
+            self.faces['B'][0] = self.faces['L'][0]
+            self.faces['L'][0] = temp
+
+            self.pastMovements.append("U")
+        else:
+            self.rotateFaces_counterclockwise('U')
+            temp = self.faces['F'][0].copy()
+
+            self.faces['F'][0] = self.faces['L'][0]
+            self.faces['L'][0] = self.faces['B'][0]
+            self.faces['B'][0] = self.faces['R'][0]
+            self.faces['R'][0] = temp
+
+            self.pastMovements.append("U'")
+
+        def D(self, inverse=False):
+        # rotates down face
+            if not inverse:
+                self.rotate_face_clockwise('D')
+                temp = self.faces['F'][2].copy()
+
+                self.faces['F'][2] = self.faces['L'][2]
+                self.faces['L'][2] = self.faces['B'][2]
+                self.faces['B'][2] = self.faces['R'][2]
+                self.faces['R'][2] = temp
+
+                self.move_history.append("D")
+            else:
+                self.rotate_face_counterclockwise('D')
+                temp = self.faces['F'][2].copy()
+
+                self.faces['F'][2] = self.faces['R'][2]
+                self.faces['R'][2] = self.faces['B'][2]
+                self.faces['B'][2] = self.faces['L'][2]
+                self.faces['L'][2] = temp
+
+                self.move_history.append("'D'")
